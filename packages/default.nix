1a2e674b5b99eb17b0faa947d52d33733949a645
Bastian Sommerfeld <bastian.sommerfeld@gmail.com>
2024-11-14 20:49:10 +0100

    feat: many

--- packages/default.nix
+++ packages/default.nix
@@ -1,7 +1,7 @@
 { pkgs }:
 {
   dmenu = pkgs.callPackage ./dmenu/pkg.nix { };
-  dwm = pkgs.callPackage ./dwm/pkg.nix { };
   dwmblocks = pkgs.callPackage ./dwmblocks/pkg.nix { };
+  dwm = pkgs.callPackage ./dwm/pkg.nix { };
   st = pkgs.callPackage ./st/pkg.nix { };
 }
